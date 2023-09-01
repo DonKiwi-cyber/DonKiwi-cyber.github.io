82454cc5c78f64ccdc8b1808e43078fae8e643fd
Ian <minecraft200294@gmail.com>
2023-09-01 10:27:02 -0600

    add vars header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  name : string = "Ian Carlo";
+  titulos : string = "ninguno";
+  objetivo : string = "convertirnme en project manager";
+  foto : string = "gs://mycv-f4d5d.appspot.com/damn_bro.jpg";
+  email : string = "zS21004532@estudiantes.uv.mx";
+  celular : string = "(272) 135 6455";
+  ubicacion : string = "Orizaba Veracruz";
+  redsocial : string = "Ian Pico";
 }
