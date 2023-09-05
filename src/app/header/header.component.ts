079b9c7e2d21831828be7714eeda7c1aa1733a84
Ian <minecraft200294@gmail.com>
2023-09-05 11:29:06 -0600

    push issue header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  name : string = "Ian Carlo";
-  titulos : string = "ninguno";
-  objetivo : string = "convertirnme en project manager";
-  foto : string = "gs://mycv-f4d5d.appspot.com/damn_bro.jpg";
-  email : string = "zS21004532@estudiantes.uv.mx";
-  celular : string = "(272) 135 6455";
-  ubicacion : string = "Orizaba Veracruz";
-  redsocial : string = "Ian Pico";
+  constructor(private headerService : HeaderService)
+  {}
+
+  name : string = "fake";
+  titulos : string = "fake";
+  objetivo : string = "fake";
+  foto : string = "fake";
+  email : string = "fake";
+  celular : string = "fake";
+  ubicacion : string = "fake";
+  facebook : string = "fake";
+
+  ngOnInit() : void {
+    this.headerService.getHeader()
+    .subscribe((data: any) => {
+	console.log(data);
+	// alert(data);
+	this.name = data.name;
+	this.titulos = data.titulos;
+	this.objetivo = data.objetivo;
+	this.foto = data.foto;
+	this.email = data.email;
+	this.celular = data.celular;
+	this.ubicacion = data.ubicacion;
+	this.facebook = data.facebook;
+    });
+  }
 }
