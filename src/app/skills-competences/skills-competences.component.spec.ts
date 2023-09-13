d6cb3e0329ba958ddea91118fd2edb7452b3d90c
Ian <minecraft200294@gmail.com>
2023-09-13 11:11:59 -0600

    fix skill-competences issue

--- src/app/skills-competences/skills-competences.component.spec.ts
+++ src/app/skills-competences/skills-competences.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkillsCompetencesComponent } from './skills-competences.component';
 
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
 describe('SkillsCompetencesComponent', () => {
   let component: SkillsCompetencesComponent;
   let fixture: ComponentFixture<SkillsCompetencesComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SkillsCompetencesComponent]
+      declarations: [SkillsCompetencesComponent],
+      imports : [HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(SkillsCompetencesComponent);
     component = fixture.componentInstance;
