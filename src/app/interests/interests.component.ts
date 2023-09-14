2d4e827d0eb59d65465e1de788204dec172cc105
Ian <minecraft200294@gmail.com>
2023-09-14 17:58:34 -0600

    quick fixes issue #14

---
+++ src/app/interests/interests.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { InterestsService } from '../services/interests.service'
+
+@Component({
+  selector: 'app-interests',
+  templateUrl: './interests.component.html',
+  styleUrls: ['./interests.component.css']
+})
+export class InterestsComponent implements OnInit{
+
+  constructor(private interestsService: InterestsService)
+  {}
+
+  arreglo: any = []
+  
+
+  ngOnInit() : void {
+    this.interestsService.getHeader()
+    .subscribe((data: any) => {
+	    console.log(data);
+	    // alert(data);
+      this.arreglo = data;
+      console.log(this.arreglo)
+    });
+  }
+}
